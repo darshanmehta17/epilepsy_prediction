f1629ebde033e40fd3ddb5c6174ae1a31b4700c1
Darshan Mehta <darshanmehta17@gmail.com>
2017-03-25 23:40:14 +0530

    Update the grammatical issue

--- utils.py
+++ utils.py
@@ -2,13 +2,14 @@ import numpy as np
 
 
 '''
-    Returns the greatest half lesser than the number 
+    Returns the greatest half lesser than the number.
 '''
 def floor_half(x):
     return 0.5 * np.floor(2.0 * x)
 
+
 '''
-    Returns the index of the value in the array
+    Returns the index of the value in the array.
 '''
 def index_of(arr, k):
     return np.nonzero(arr == k)[0][0]
